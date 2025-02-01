db0b66330b9fcb72d7e8c165dbc520038703d6e9
Varad <varadbhagwat05@gmail.com>
2025-02-01 22:16:22 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require("express")
+const https = require("https")
+const bodyParser = require("body-parser")
+const request = require("request")
+
+const app = express()
+// app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get("/signup", function(req, res){
+    res.sendFile(__dirname + "/signup.html")
+})
+
+app.post("/signup", function(req, res){
+    const fname = req.body.firstName
+    const lname = req.body.lastName
+    const email = req.body.email
+    
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: fname,
+                    LNAME: lname
+                }
+            }
+        ]
+    }
+    const JSONdata = JSON.stringify(data)
+
+    const url = "https://us15.api.mailchimp.com/3.0/lists/b16d70220c"
+
+    const options = {
+        method: "POST",
+        auth: "varad:b254d0113a490eeedfa15958871f4a36-us15"
+    }
+
+    const request = https.request(url, options, function(response){
+
+        if( response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html")
+        }else{
+            res.sendFile(__dirname + "/failure.html")
+        }
+
+        response.on("data", function(data){
+            console.log(JSON.parse(data))
+        })
+    })
+
+    request.write(JSONdata)
+    request.end()
+})
+
+app.post("/failure", function(req, res){
+    res.redirect("/signup")
+})
+
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Server started on port 3000.")
+})
+
+// Audience ID
+// b16d70220c
+
+// API key
+// b254d0113a490eeedfa15958871f4a36-us15
